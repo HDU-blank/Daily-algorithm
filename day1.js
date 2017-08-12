50f44bf4b6c007df40d4fb74f29bcdc33add4f0c
HDU-blank <2583066474@qq.com>
2017-08-12 10:24:26 +0800

    modify

---
+++ day1.js
@@ -0,0 +1,44 @@
+function countIslands(arr){
+    var counts = 0,
+        N = arr.length,
+        M = arr[0].length;
+    if (arr == null || arr[0] == null) {
+            return 0;
+        }
+    for (var i = 0;i < arr.length;i++){
+        for (var j = 0;j < arr[0].length;j++){
+            if (arr[i][j] == 1){
+                counts ++;
+                infect(arr,i,j,N,M);
+            }
+        }
+    }
+    return counts;
+}
+
+function infect(arr,i,j,N,M){
+    if (i < 0 || i >= N || j < 0 || j >= M ||arr[i][j] != 1){
+        return ;
+    }
+    arr[i][j] = 2;
+    infect(arr,i+1,j,N,M);
+    infect(arr,i-1,j,N,M);
+    infect(arr,i,j+1,N,M);
+    infect(arr,i,j-1,N,M);
+}
+
+var array1=[ [ 0,0,0,0,0,0,0,0,0 ], 
+             [ 0,1,1,1,0,1,1,1,0 ], 
+             [ 0,1,1,1,0,0,0,1,0 ],
+             [ 0,1,1,0,0,0,0,0,0 ], 
+             [ 0,0,0,0,0,1,1,0,0 ], 
+             [ 0,0,0,0,1,1,1,0,0 ],
+             [ 0,0,0,0,0,0,0,0,0 ] ],
+    array2=[ [ 0,0,0,0,0,0,0,0,0 ],
+             [ 0,1,1,0,1,1,1,1,0 ],
+             [ 0,1,1,1,1,0,0,1,0 ],
+             [ 0,1,1,0,0,0,0,1,0 ],
+             [ 0,0,0,0,0,1,1,1,0 ],
+             [ 0,0,0,0,1,1,1,0,0 ],
+             [ 0,0,0,0,0,0,0,0,0 ] ];
+countIslands(array1);
