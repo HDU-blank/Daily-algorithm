52ef73184dc30dbe66093c51f50d0efbb5880bc0
HDU-blank <2583066474@qq.com>
2017-08-19 20:36:08 +0800

    modify

--- day7.js
+++ day7.js
@@ -1,7 +1,22 @@
-//非递归
-function Fibonacci(n)
-{
-    if(n == 0) {
+//非递归-1
+function Fibonacci(n){
+    if (n < 0) {
+        throw new Error('输入的数字不能小于0');
+    }
+    var prev = 0,
+        cur = 1;
+    while ( n-- ){
+        cur += prev;
+        prev = cur-prev;
+    }
+    return prev;
+}
+//非递归-2
+function Fibonacci(n){
+    if (n < 0) {
+        throw new Error('输入的数字不能小于0');
+    }
+    else if(n == 0) {
         return 0;
     }else if(n == 1) {
         return 1;
@@ -12,6 +27,17 @@ function Fibonacci(n)
     }
     return fibona.pop();
 }
-
 //递归
-
+function Fibonacci(n){
+    if (n < 0) {
+        throw new Error('输入的数字不能小于0');
+    }
+    else if(n == 0) {
+        return 0;
+    }else if(n == 1) {
+        return 1;
+    }
+    else{
+        return Fibonacci(n-2)+Fibonacci(n-1);
+    }
+}
